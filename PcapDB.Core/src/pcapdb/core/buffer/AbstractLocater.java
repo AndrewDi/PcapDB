d10be51097233894ed872f2310dacdaeb86ac516
Andrew <pandipd@outlook.com>
2018-08-09 22:33:47 +0800

    init

--- PcapDB.Core/src/pcapdb/core/buffer/AbstractLocater.java
+++ PcapDB.Core/src/pcapdb/core/buffer/AbstractLocater.java
@@ -1,6 +1,6 @@
 package pcapdb.core.buffer;
 
-public interface AbstractLocater {
+public abstract class AbstractLocater {
 
 
 }
--- PcapDB.Core/src/pcapdb/core/buffer/MappedByteBufferLocater.java
+++ PcapDB.Core/src/pcapdb/core/buffer/MappedByteBufferLocater.java
@@ -1,4 +1,45 @@
 package pcapdb.core.buffer;
 
-public class MappedByteBufferLocater implements AbstractLocater {
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+
+public class MappedByteBufferLocater extends AbstractLocater {
+    private MappedByteBuffer mappedByteBuffer;
+
+    private int baseOffset;
+    private int currentRelativeOffset;
+
+    public MappedByteBufferLocater(MappedByteBuffer _mappedByteBuffer, int _baseOffset, int _currentRelativeOffset){
+        this.mappedByteBuffer=_mappedByteBuffer;
+        this.baseOffset=_baseOffset;
+        this.currentRelativeOffset=_currentRelativeOffset;
+    }
+
+    public int getRemainLength(){
+        return this.mappedByteBuffer.array().length-this.mappedByteBuffer.arrayOffset();
+    }
+
+    public int getInt(){
+        return this.mappedByteBuffer.getInt();
+    }
+
+    public short getShort(){
+        return this.mappedByteBuffer.getShort();
+    }
+
+    public long getLong(){
+        return this.mappedByteBuffer.getLong();
+    }
+
+    public ByteBuffer getByteBuffer(byte[] bytes, int offset,int length){
+        return this.mappedByteBuffer.get(bytes,offset,length);
+    }
+
+    public char getChar(){
+        return this.mappedByteBuffer.getChar();
+    }
+
+    public MappedByteBufferLocater getPayload(){
+        return new MappedByteBufferLocater(this.mappedByteBuffer,this.currentRelativeOffset,0);
+    }
 }
